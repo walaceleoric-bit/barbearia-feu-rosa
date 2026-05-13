4d468c35687685830ea78ffeea25a2214d577c6c
Walace <walaceleoric@gmail.com>
2026-05-13 20:07:01 -0300

    Correção avaliação

--- BarbeariaFeuRosa/Controllers/HistoricoClienteController.cs
+++ BarbeariaFeuRosa/Controllers/HistoricoClienteController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BarbeariaFeuRosa.Data;
+using BarbeariaFeuRosa.Models;
 
 namespace BarbeariaFeuRosa.Controllers
 {
@@ -40,7 +41,120 @@ namespace BarbeariaFeuRosa.Controllers
                 .OrderByDescending(a => a.DataHora)
                 .ToList();
 
+            var idsAvaliados = _context.AvaliacoesBarbeiros
+                .Where(a => a.BarbeariaId == barbeariaId.Value)
+                .Select(a => a.BarbeiroId + "-" + a.ClienteId)
+                .ToList();
+
+            ViewBag.AvaliacoesFeitas = idsAvaliados;
+
             return View(historico);
+        }
+
+        public IActionResult Avaliar(int id)
+        {
+            if (HttpContext.Session.GetString("UsuarioTipo") != "CLIENTE")
+                return RedirectToAction("Login", "Auth");
+
+            var barbeariaId = ObterBarbeariaId();
+
+            if (barbeariaId == null)
+                return RedirectToAction("Login", "Auth");
+
+            var usuarioNome = HttpContext.Session.GetString("UsuarioNome");
+
+            var agendamento = _context.Agendamentos
+                .Include(a => a.Cliente)
+                .Include(a => a.Barbeiro)
+                .FirstOrDefault(a =>
+                    a.Id == id &&
+                    a.BarbeariaId == barbeariaId.Value &&
+                    a.Status == "Finalizado" &&
+                    a.Cliente != null &&
+                    a.Cliente.Nome == usuarioNome);
+
+            if (agendamento == null)
+            {
+                TempData["Erro"] = "Este atendimento não pode ser avaliado.";
+                return RedirectToAction("Index");
+            }
+
+            bool jaAvaliou = _context.AvaliacoesBarbeiros.Any(a =>
+                a.BarbeariaId == barbeariaId.Value &&
+                a.BarbeiroId == agendamento.BarbeiroId &&
+                a.ClienteId == agendamento.ClienteId);
+
+            if (jaAvaliou)
+            {
+                TempData["Erro"] = "Você já avaliou este barbeiro.";
+                return RedirectToAction("Index");
+            }
+
+            return View(agendamento);
+        }
+
+        [HttpPost]
+        public IActionResult Avaliar(int agendamentoId, int nota, string comentario)
+        {
+            if (HttpContext.Session.GetString("UsuarioTipo") != "CLIENTE")
+                return RedirectToAction("Login", "Auth");
+
+            var barbeariaId = ObterBarbeariaId();
+
+            if (barbeariaId == null)
+                return RedirectToAction("Login", "Auth");
+
+            var usuarioNome = HttpContext.Session.GetString("UsuarioNome");
+
+            var agendamento = _context.Agendamentos
+                .Include(a => a.Cliente)
+                .Include(a => a.Barbeiro)
+                .FirstOrDefault(a =>
+                    a.Id == agendamentoId &&
+                    a.BarbeariaId == barbeariaId.Value &&
+                    a.Status == "Finalizado" &&
+                    a.Cliente != null &&
+                    a.Cliente.Nome == usuarioNome);
+
+            if (agendamento == null)
+            {
+                TempData["Erro"] = "Atendimento inválido.";
+                return RedirectToAction("Index");
+            }
+
+            if (nota < 1 || nota > 5)
+            {
+                TempData["Erro"] = "Escolha uma nota de 1 a 5.";
+                return RedirectToAction("Avaliar", new { id = agendamentoId });
+            }
+
+            bool jaAvaliou = _context.AvaliacoesBarbeiros.Any(a =>
+                a.BarbeariaId == barbeariaId.Value &&
+                a.BarbeiroId == agendamento.BarbeiroId &&
+                a.ClienteId == agendamento.ClienteId);
+
+            if (jaAvaliou)
+            {
+                TempData["Erro"] = "Você já avaliou este barbeiro.";
+                return RedirectToAction("Index");
+            }
+
+            var avaliacao = new AvaliacaoBarbeiro
+            {
+                BarbeariaId = barbeariaId.Value,
+                BarbeiroId = agendamento.BarbeiroId,
+                ClienteId = agendamento.ClienteId,
+                Nota = nota,
+                Comentario = comentario ?? "",
+                DataAvaliacao = DateTime.UtcNow
+            };
+
+            _context.AvaliacoesBarbeiros.Add(avaliacao);
+            _context.SaveChanges();
+
+            TempData["Sucesso"] = "Avaliação enviada com sucesso!";
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Excluir(int id)
